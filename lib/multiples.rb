f9ef5eca162814b4e61f217bf1554a01da794152
Alan Hong <xiupan@gmail.com>
2018-10-23 11:06:20 -0500

    Woot woot

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,17 @@
 # Enter your procedural solution here!
+def collect_multiples(input_num)
+  temp_array = []
+  input_num.times do |i|
+    if i % 3 == 0 && i != 0
+      temp_array << i
+    elsif i % 5 == 0 && i != 0
+      temp_array << i
+    end
+  end
+  temp_array
+end
+
+def sum_multiples(input_num)
+  temp_array = collect_multiples(input_num)
+  temp_array.inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,27 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  attr_accessor :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def collect_multiples
+    temp_array = []
+    @number.times do |i|
+      if i % 3 == 0 && i != 0
+        temp_array << i
+      elsif i % 5 == 0 && i != 0
+        temp_array << i
+      end
+    end
+    temp_array
+  end
+
+  def sum_multiples
+    # self.collect_multiples.inject(0){|sum,x| sum + x }
+    self.collect_multiples.inject(:+)
+  end
+
+end
